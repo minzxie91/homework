6eaef4b6f2142ae4fc72552eb96d275af8b5d3c4
minzxie91 <s9105091@gmail.com>
2024-06-17 23:43:56 +0800

    Add files via upload

--- homework.py
+++ homework.py
@@ -1,24 +1,25 @@
 import requests
 from bs4 import BeautifulSoup
-# word = input( '請輸入中文字:' )
+# word = input( '請輸入英文字:' )
     
 def read( word ):   
-    url = f'https://dict.revised.moe.edu.tw/search.jsp?md=1&word={word}#searchL'
+    url = f'https://tw.dictionary.search.yahoo.com/search;_ylt=AwrtiZwrT3Bmxysilxx7rolQ;_ylc=X1MDMTM1MTIwMDM3OQRfcgMyBGZyAwRmcjIDc2ItdG9wLXNlYXJjaARncHJpZANtSU9xUFFNYlNIYXV6YUZJdW1EX19BBG5fcnNsdAMwBG5fc3VnZwMxMARvcmlnaW4DdHcuZGljdGlvbmFyeS5zZWFyY2gueWFob28uY29tBHBvcwMwBHBxc3RyAwRwcXN0cmwDMARxc3RybAM0BHF1ZXJ5A2hvcGUEdF9zdG1wAzE3MTg2MzYzMzg-?p={word}&fr=sfp&iscqry=&fr2=sb-top-search'
+
 
 
     html = requests.get( url )
     bs = BeautifulSoup(html.text,'lxml')
-    data = bs.find('table', id='searchL')
+    data = bs.find('div', class_="layoutCenter")
     try:
-        row = data.find_all('tr')[2]
-        chinese = row.find('cr').text
-        phones = row.find_all('code')
+        row = data.find_all('li')[0]
+        english = row.find('span').text
+        phones = row.find_all('span')
         phone = [e.text for e in phones]
         s = " ".join( phone )
         # s = row.find('sub')
-        return( chinese + ' => ' + s )
+        return( english + ' => ' + s )
     except:
         return( '查無此字' )
     data
         
-read('國小')
+read('wish')
